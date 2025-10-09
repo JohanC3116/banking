44b51516476e964e65d477f9442e7e80fb0d6041
Johan Camilo Calderon Alvarez <jjcalderon.4697@unicesmag.edu.co>
2025-10-08 21:14:21 -0500

    Country templates for UI

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,25 +1,16 @@
-#from django.urls import path
-#from . import views
+from django.urls import path
+from . import views
 
-#urlpatterns = (
-  #  path("", views.index, name="index"),
-#)
+app_name = "authentication_app"
 
-#urlpatterns = (
- #  path("", views.country, name="country"),
-#)
+urlpatterns=[
+    path("",views.index,name="index"),
+    #path("countries",views.countries,name="countries"),
+    #path("depart",views.depart,name="departments"),
+    #path("cities",views.cities,name="cities")
+    path("countries/", views.country_list, name="country_list"),
+    path("countries/<int:pk>/edit/", views.country_edit, name="country_edit"),
+    path('countries/delete/<int:pk>/', views.country_delete, name='country_delete'),
 
-#urlpatterns = (
- #  path("", views.city, name="city"),
-#)
-
-from django.urls import path 
-from . import views 
-
-app_name = 'banking' 
-
-urlpatterns = [ 
-   path('countries/', views.country_list, name='country_list'), 
-   path('countries/<int:pk>/edit/', views.country_edit, name='country_edit'), 
-] 
+]
 
--- authentication/views.py
+++ authentication/views.py
@@ -1,32 +1,42 @@
-""" from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from .models import Country
+from .forms import CountryForm
+
+from django .http import HttpResponse
+
 # Create your views here.
 def index(request):
-    return HttpResponse("This is my first view !!!")
+    return HttpResponse("")
 
-def country(request):
-    return HttpResponse("Country !!!")
+def country_list(request):
+    countries = Country.objects.all().order_by('name')
+    return render(request, "authentication_app/country_list.html", {"countries": countries})
 
-def city(request):
-    return HttpResponse("City !!!") """
+def country_edit(request, pk):
+    country = get_object_or_404(Country, pk=pk)
 
-from django.shortcuts import render, get_object_or_404, redirect 
-from django.contrib import messages 
-from .models import Country 
-from .forms import CountryForm 
+    if request.method == 'POST':
+        form = CountryForm(request.POST, instance=country)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'País actualizado correctamente.')
+            return redirect('authentication:country_list')
+    else:
+        form = CountryForm(instance=country)  
 
-def country_list(request): 
-    countries = Country.objects.all().order_by('name') 
-    return render(request, 'banking_app/country_list.html', {'countries': countries}) 
+    return render(request, "authentication_app/country_form.html", {
+        "form": form,
+        "country": country
+    })
+    
+def country_delete(request, pk):
+    country = get_object_or_404(Country, pk=pk)
+    if request.method == "POST":
+        country.delete()
+        messages.success(request, "El país fue eliminado correctamente.")
+        return redirect('authentication_app:country_list') 
+    return redirect('authentication_app:country_list')
 
-def country_edit(request, pk): 
-    country = get_object_or_404(Country, pk=pk) 
-    if request.method == 'POST': 
-        form = CountryForm(request.POST, instance=country) 
-        if form.is_valid(): 
-            form.save() 
-            messages.success(request, 'País actualizado correctamente.') 
-            return redirect('country_list') 
-    else: 
-        form = CountryForm(instance=country) 
-    return render(request, 'banking_app/country_form.html', {'form': form, 'country': country}) 
+
+
